ab7a4c4ee6af935ebabb3f9f35f4c7a7890bda15
aser-amr <97066286+aser-amr@users.noreply.github.com>
2023-04-14 14:38:41 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,29 +3,53 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     radio.sendString("ButtonE_Pressed")
+    basic.showString("E")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.up, function () {
     radio.sendString("ButtonD_Released")
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("ButtonA_Pressed")
+    basic.showString("A")
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     radio.sendString("ButtonF_Pressed")
+    basic.showString("F")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.up, function () {
     radio.sendString("ButtonF_Released")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("ButtonAB_Pressed")
+    basic.showString("AB")
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Shake") {
         joystickbit.Vibration_Motor(200)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("ButtonB_Pressed")
+    basic.showString("B")
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.up, function () {
     radio.sendString("ButtonE_Released")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radio.sendString("ButtonD_Pressed")
+    basic.showString("D")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     radio.sendString("ButtonC_Pressed")
+    basic.showString("C")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("Logo_Pressed")
+    basic.showIcon(IconNames.Happy)
+})
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    radio.sendString("Logo_Released")
+    basic.showIcon(IconNames.Sad)
 })
 radio.setGroup(1)
 joystickbit.initJoystickBit()
